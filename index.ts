d2afe5ac488c8e04aab5df727549cfb6ca0b8a27
Leonardoggoes <127688040+Leonardoggoes@users.noreply.github.com>
2023-04-19 11:46:08 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const possibilidadesVitoria = [
   [3, 6, 9],
   // diagonais
   [1, 5, 9],
-  [3, 5, 7]
+  [3, 5, 7],
 ];
 
 const myButtons = document.querySelectorAll('.option');
@@ -35,34 +35,40 @@ function buttonClick(event) {
   } else if (vez == 'o') {
     obotao.classList.add('optiono');
     adicionar(obotao.id);
-    verificarVitoria()
+    verificarVitoria();
     vez = 'x';
   }
 }
 
 function adicionar(any) {
-  if (vez == 'x') {locaisx += any;}
-  else if (vez == 'o') {locaisy += any;}
+  if (vez == 'x') {
+    locaisx += any;
+  } else if (vez == 'o') {
+    locaisy += any;
+  }
 }
 
-function verificarVitoria(){
-
-console.log("locaisX:" + locaisx);
-console.log("locaisO:" + locaisy);
+function verificarVitoria() {
   for (let i = 0; i < possibilidadesVitoria.length; i++) {
     const [a, b, c] = possibilidadesVitoria[i];
     if (locaisx.includes(a) && locaisx.includes(b) && locaisx.includes(c)) {
-      console.log("o jogador X venceu");
+      console.log('o jogador X venceu');
+      finalizarJogo();
       return;
-    } else if (locaisy.includes(a) && locaisy.includes(b) && locaisy.includes(c)) {
-      console.log("o jogador O venceu");
+    } else if (
+      locaisy.includes(a) &&
+      locaisy.includes(b) &&
+      locaisy.includes(c)
+    ) {
+      console.log('o jogador O venceu');
+      finalizarJogo();
       return;
     }
   }
-
 }
 
+function finalizarJogo() {
+  const tabuleiro = document.querySelector('.areajogo');
+  tabuleiro.classList.add('finalizarjogo');
 
-function finalizarJogo(){
-  
 }
